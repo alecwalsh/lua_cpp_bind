205d3a247a7ad5b5d4d2f65644ea581043ea36a7
Alec Walsh <alecwalsh1996@gmail.com>
2018-02-23 20:38:22 -0500

    Move headers to include directory

---
+++ include/LuaValue.h
@@ -0,0 +1,45 @@
+#pragma once
+#if __cplusplus >= 201703L
+#include <any>
+using std::any;
+using std::any_cast;
+#else
+#include <boost/any.hpp>
+using boost::any;
+using boost::any_cast;
+#endif
+
+#include "LuaScript.h"
+
+struct LuaValue {
+    int type;
+    any value;
+    
+    //TODO: add more types
+    bool operator==(const LuaValue& rhs) const noexcept;
+    
+    std::string str() const;
+};
+
+std::ostream& operator<<(std::ostream& o, const LuaValue& lv);
+
+namespace std {
+    template<> struct hash<LuaValue> {
+        using argument_type = LuaValue;
+        using result_type = std::size_t;
+        
+        //TODO: implement this
+        std::size_t operator()(const LuaValue& lv) const noexcept {
+            switch(lv.type) {
+                case LUA_TNUMBER:
+                    return std::hash<LUA_NUMBER>()(any_cast<LUA_NUMBER>(lv.value));
+                case LUA_TBOOLEAN:
+                    return std::hash<bool>()(any_cast<bool>(lv.value));
+                case LUA_TSTRING:
+                    return std::hash<std::string>()(any_cast<std::string>(lv.value));
+                default:
+                    return 0;
+            }
+        }
+    };
+}
